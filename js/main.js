9c8467596543e48f0a71a22bf1ca3ebbedbdfe16
J <asdfg_john@yahoo.com>
2024-07-01 19:52:41 -0500

    added town image

--- js/main.js
+++ js/main.js
@@ -7,6 +7,7 @@ let imgPromises = [];
 
 GraphicsEngine.setBackgroundColor('#9bbc0f')
 imgPromises.push(GraphicsEngine.loadImage('/img/roads.png', 'roads'));
+imgPromises.push(GraphicsEngine.loadImage('/img/house.png', 'house'));
 
 Promise.all(imgPromises).then(() => {
 
@@ -14,10 +15,12 @@ Promise.all(imgPromises).then(() => {
   let r2 = new GraphicsEngine.GraphicsObject();
   let r3 = new GraphicsEngine.GraphicsObject();
   let r4 = new GraphicsEngine.GraphicsObject();
+  let h1 = new GraphicsEngine.GraphicsObject();
   r1.setSprites(0, GraphicsEngine.createSprite('roads', 0, 32, 32, 32));
   r2.setSprites(0, GraphicsEngine.createSprite('roads', 0, 32, 32, 32, true, false));
   r3.setSprites(0, GraphicsEngine.createSprite('roads', 0, 32, 32, 32, false, true));
   r4.setSprites(0, GraphicsEngine.createSprite('roads', 0, 32, 32, 32, true, true));
+  h1.setSprites(0, GraphicsEngine.createSprite('house', 0, 0, 32, 32));
 
   
   GraphicsEngine.resizeCanvas(800, 600);
@@ -27,4 +30,5 @@ Promise.all(imgPromises).then(() => {
   GraphicsEngine.drawObject(r2, 64, 32);
   GraphicsEngine.drawObject(r3, 32, 64);
   GraphicsEngine.drawObject(r4, 64, 64);
+  GraphicsEngine.drawObject(h1, 128, 128)
 })
